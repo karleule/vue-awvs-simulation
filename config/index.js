810be604e20e3ac08a831e425fa82f0a302201ca
Daniel Schulz <karleule@posteo.de>
2018-03-28 02:34:47 +0200

    bugfixed assests public path, moved from dev to build

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-awvs-simulation/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-awvs-simulation/',
 
     /**
      * Source Maps
